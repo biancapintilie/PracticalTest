c45be3e42138d912038f301496a549188e86f4a6
biancapintilie <bianca.pintilie@stud.acs.pub.ro>
2025-11-11 19:57:50 +0200

    rezolvare A.1 si A.2

--- PracticalTest01Var062/app/src/main/java/com/example/practicaltest01var06/MainActivity.kt
+++ PracticalTest01Var062/app/src/main/java/com/example/practicaltest01var06/MainActivity.kt
@@ -1,21 +1,16 @@
 package com.example.practicaltest01var06
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.CheckBox
 import android.util.Log
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
-//    private lateinit var input: EditText
-//    private lateinit var button: Button
-//    private lateinit var checkbox: CheckBox
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,23 +22,34 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val edit1 = findViewById<EditText>(R.id.editText1)
-        val edit2 = findViewById<EditText>(R.id.editText2)
-        val edit3 = findViewById<EditText>(R.id.editText3)
+        val edit1 = findViewById<TextView>(R.id.editText1)
+        val edit2 = findViewById<TextView>(R.id.editText2)
+        val edit3 = findViewById<TextView>(R.id.editText3)
         val btn1 = findViewById<Button>(R.id.play_button)
         val btn2 = findViewById<Button>(R.id.points_button)
         val cb1 = findViewById<CheckBox>(R.id.checkBox1)
         val cb2 = findViewById<CheckBox>(R.id.checkBox2)
         val cb3 = findViewById<CheckBox>(R.id.checkBox3)
 
-        btn1.setOnClickListener {
-            var score = 0
-            if (cb1.isChecked) score = score + edit1.text.toString().toInt()
-            if (cb2.isChecked) score = score + edit2.text.toString().toInt()
-            if (cb3.isChecked) score = score + edit3.text.toString().toInt()
-            val rezultat = score.toString()
-            Log.d("MainActivity", "Rezultat Action1: $rezultat")
-        }
+//        val buttonClickListener: ButtonClickListener = ButtonClickListener()
+
+//        btn1.setOnClickListener {
+//            var score = 0
+//            if (cb1.isChecked) score = score + edit1.text.toString().toInt()
+//            if (cb2.isChecked) score = score + edit2.text.toString().toInt()
+//            if (cb3.isChecked) score = score + edit3.text.toString().toInt()
+//            val rezultat = score.toString()
+//            Log.d("MainActivity", "Rezultat Action1: $rezultat")
+//        }
+
+//        btn1.setOnClickListener {
+//
+//            val mResult = ("*, 1, 2, 3").random()
+//
+//            if (cb1.isChecked) edit1.text = mResult.toString()
+//            if (cb2.isChecked) edit2.text = mResult.toString()
+//            if (cb3.isChecked) edit3.text = mResult.toString()
+//        }
 
     }
 }
